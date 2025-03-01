e91d51ade36d5f404fcf9c9299980b368e37e12c
Rut26 <rutpurohit0@gmail.com>
2025-03-01 10:23:44 +0530

    Initialization

--- Assignment/src/app/app.routes.ts
+++ Assignment/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponentComponent } from './home-component/home-component.component';
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path:"home", component: HomeComponentComponent},
+];
